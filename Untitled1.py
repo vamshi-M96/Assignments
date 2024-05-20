e137b53c3138e7cdd8b0e3a41ea4acdfd623e620
vamshi-M96 <vamsi.meka96@gmail.com>
2024-05-20 12:06:05 +0530

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import streamlit as st
+
+
+# In[2]:
+
+
+st.title('Vamshi')
+
+
+# In[ ]:
+
+
+st.write('Its working')
+
